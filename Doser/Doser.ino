506a16803c4b9593a6728164ab8002befba60ef9
Eli <elimbukoski@gmail.com>
2022-07-16 22:14:52 -0400

    fix runtime bugs

--- Doser/Doser.ino
+++ Doser/Doser.ino
@@ -1,7 +1,7 @@
-int Total_Print_Volume = 401; //mililiters
-long Total_Print_Time = 15000; //seconds
-int Number_Of_Doses = 10; //times pump is turned on
-float Dose_Flow_Rate = 0.5; //ml/s
+int Total_Print_Volume = 236; //mililiters
+long Total_Print_Time = 240; //seconds
+int Number_Of_Doses = 1; //times pump is turned on
+float Dose_Flow_Rate = 1; //ml/s
 bool default_dir = true; //flip what direction "forward" is
 
 float dose_volume = Total_Print_Volume / Number_Of_Doses;
@@ -14,86 +14,86 @@ int doses_count = 0;
 
 byte state = 0;// 0 = off, 1 = manual, 2 = dosing off time, 3 = dosing
 
-byte start = 13; //pin for start button
-byte estop = 12; //pin for estop button
-byte forward = 11; //pin for forward button
-byte backward = 10; //pin for backward button
-byte manual = 9; //pin for manual light
-byte dosing = 8; //pin for dosing light
+byte start = 3; //pin for start button
+byte estop = 2; //pin for estop button
+byte forward = 5; //pin for forward button
+byte backward = 4; //pin for backward button
+byte manual = 8; //pin for manual light
+byte dosing = 6; //pin for dosing light
 byte off = 7; //pin for off light
-byte mstep = 5; //pin for mstep motor
-byte dir = 6; //pin for direction motor
-byte enable = 4; //pin for enable motor
+byte mstep = 9; //pin for mstep motor
+byte dir = 10; //pin for direction motor
+byte enable = 11; //pin for enable motor
 
 void setup()
 {
-  pinMode(start, INPUT);
-  pinMode(estop, INPUT);
-  pinMode(forward, INPUT);
-  pinMode(backward, INPUT);
+  pinMode(start, INPUT_PULLUP);
+  pinMode(estop, INPUT_PULLUP);
+  pinMode(forward, INPUT_PULLUP);
+  pinMode(backward, INPUT_PULLUP);
   pinMode(manual, OUTPUT);
   pinMode(dosing, OUTPUT);
   pinMode(off, OUTPUT);
   pinMode(mstep, OUTPUT);
   pinMode(dir, OUTPUT);
   pinMode(enable, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (state = 0) { //off
+  Serial.println(state);
+  if (state == 0) { //off
     digitalWrite(manual, LOW);
     digitalWrite(dosing, LOW);
     digitalWrite(off, HIGH);
-    if (digitalRead(start)) {
+    digitalWrite(enable, HIGH);
+    if (!digitalRead(start)) {
       state = 2;
       timer = 0;
       doses_count = 0;
-      digitalWrite(enable, HIGH);
     }
-    if (digitalRead(forward)) {
+    if (!digitalRead(forward)) {
       state = 1;
-      digitalWrite(enable, HIGH);
     }
-    if (digitalRead(backward)) {
+    if (!digitalRead(backward)) {
       state = 1;
-      digitalWrite(enable, HIGH);
     }
   }
-  else if (state = 1) { //manual
+  else if (state == 1) { //manual
     digitalWrite(manual, HIGH);
     digitalWrite(dosing, LOW);
     digitalWrite(off, LOW);
-    if (digitalRead(estop)) {
+    digitalWrite(enable, LOW);
+    if (!digitalRead(estop)) {
       state = 0;
-      digitalWrite(enable, LOW);
       return;
     }
-    if (digitalRead(forward)) {
+    if (!digitalRead(forward)) {
       digitalWrite(dir, default_dir);
       do {
         digitalWrite(mstep, HIGH);
-        delay(10);
+        delay(1);
         digitalWrite(mstep, LOW);
-        delay(10);
-      } while (digitalRead(forward));
+        delay(1);
+      } while (!digitalRead(forward));
     }
-    if (digitalRead(backward)) {
-      digitalWrite(dir, default_dir);
+    if (!digitalRead(backward)) {
+      digitalWrite(dir, !default_dir);
       do {
         digitalWrite(mstep, HIGH);
-        delay(10);
+        delay(1);
         digitalWrite(mstep, LOW);
-        delay(10);
-      } while (digitalRead(backward));
+        delay(1);
+      } while (!digitalRead(backward));
     }
   }
-  else if (state = 2) { //dosing off time
+  else if (state == 2) { //dosing off time
     digitalWrite(manual, LOW);
     digitalWrite(dosing, HIGH);
     digitalWrite(off, HIGH);
-    if (digitalRead(estop)) {
+    digitalWrite(enable, HIGH);
+    if (!digitalRead(estop)) {
       state = 0;
-      digitalWrite(enable, LOW);
       return;
     }
     if (timer <= off_time) {
@@ -106,11 +106,11 @@ void loop() {
     }
 
   }
-  if (state = 3) { //dosing
+  if (state == 3) { //dosing
     digitalWrite(manual, LOW);
     digitalWrite(dosing, HIGH);
     digitalWrite(off, LOW);
-
+    digitalWrite(enable, LOW);
     if (timer <= dose_time) {
       timer++;
       digitalWrite(dir, default_dir);
@@ -121,9 +121,8 @@ void loop() {
         delay(10);
         pulse++;
 
-        if (digitalRead(estop)) {
+        if (!digitalRead(estop)) {
           state = 0;
-          digitalWrite(enable, LOW);
           return;
         }
       } while (pulse < 100);

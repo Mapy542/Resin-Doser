db68e48812a8f8f1324a061054468a5034cad868
Eli <elimbukoski@gmail.com>
2022-08-19 17:11:59 -0400

    fixed dose params and improved format

--- Doser/Doser.ino
+++ Doser/Doser.ino
@@ -1,35 +1,37 @@
-//USER-----
-int Total_Print_Volume = 427; //mililiters
-long Total_Print_Time = 34440; //seconds
-int Number_Of_Doses = 30; //times pump is turned on
-float Dose_Flow_Rate = .45; //ml/s
-bool default_dir = true; //flip what direction "forward" is
-//----------
 
+
+// USER-----
+int Total_Print_Volume = 427;  // mililiters
+long Total_Print_Time = 34440; // seconds
+int Number_Of_Doses = 30;      // times pump is turned on
+float Dose_Flow_Rate = .225;   // ml/s
+bool default_dir = true;       // flip what direction "forward" is
+//----------
 
 float dose_volume = (float)Total_Print_Volume / Number_Of_Doses;
 long dose_time = round(dose_volume / Dose_Flow_Rate);
-long off_time = round(((float)Total_Print_Time - ((float)Number_Of_Doses*dose_time)) / Number_Of_Doses);
+long off_time =
+    round(((float)Total_Print_Time - ((float)Number_Of_Doses * dose_time)) /
+          Number_Of_Doses);
 
 long timer = 0;
 long pulse = 0;
 int doses_count = 0;
 
-byte state = 0;// 0 = off, 1 = manual, 2 = dosing off time, 3 = dosing
+byte state = 0; // 0 = off, 1 = manual, 2 = dosing off time, 3 = dosing
 
-byte start = 3; //pin for start button
-byte estop = 2; //pin for estop button
-byte forward = 5; //pin for forward button
-byte backward = 4; //pin for backward button
-byte manual = 8; //pin for manual light
-byte dosing = 6; //pin for dosing light
-byte off = 7; //pin for off light
-byte mstep = 9; //pin for mstep motor
-byte dir = 10; //pin for direction motor
-byte enable = 11; //pin for enable motor
+byte start = 3;    // pin for start button
+byte estop = 2;    // pin for estop button
+byte forward = 5;  // pin for forward button
+byte backward = 4; // pin for backward button
+byte manual = 8;   // pin for manual light
+byte dosing = 6;   // pin for dosing light
+byte off = 7;      // pin for off light
+byte mstep = 9;    // pin for mstep motor
+byte dir = 10;     // pin for direction motor
+byte enable = 11;  // pin for enable motor
 
-void setup()
-{
+void setup() {
   pinMode(start, INPUT_PULLUP);
   pinMode(estop, INPUT_PULLUP);
   pinMode(forward, INPUT_PULLUP);
@@ -47,7 +49,7 @@ void setup()
 }
 
 void loop() {
-  if (state == 0) { //off
+  if (state == 0) { // off
     digitalWrite(manual, LOW);
     digitalWrite(dosing, LOW);
     digitalWrite(off, HIGH);
@@ -64,8 +66,7 @@ void loop() {
       state = 1;
     }
     delay(100);
-  }
-  else if (state == 1) { //manual
+  } else if (state == 1) { // manual
     digitalWrite(manual, HIGH);
     digitalWrite(dosing, LOW);
     digitalWrite(off, LOW);
@@ -94,7 +95,7 @@ void loop() {
     }
   }
 
-  else if (state == 2) { //dosing off time
+  else if (state == 2) { // dosing off time
     digitalWrite(manual, LOW);
     digitalWrite(dosing, HIGH);
     digitalWrite(off, HIGH);
@@ -108,14 +109,12 @@ void loop() {
       Serial.print("OFF time remaining: ");
       Serial.println(off_time - timer);
       delay(1000);
-    }
-    else {
+    } else {
       state = 3;
       timer = 0;
     }
-
   }
-  if (state == 3) { //dosing
+  if (state == 3) { // dosing
     digitalWrite(manual, HIGH);
     digitalWrite(dosing, HIGH);
     digitalWrite(off, LOW);
@@ -125,7 +124,7 @@ void loop() {
       timer++;
       Serial.print("DOSE time remaining: ");
       Serial.println(dose_time - timer);
-      do { //based on 10milisec pules on mstepper
+      do { // based on 10milisec pules on mstepper
         digitalWrite(mstep, HIGH);
         delay(1);
         digitalWrite(mstep, LOW);
@@ -138,16 +137,13 @@ void loop() {
         }
       } while (pulse < 500);
       pulse = 0;
-    }
-    else {
+    } else {
       state = 2;
       timer = 0;
       doses_count++;
       if (doses_count == Number_Of_Doses) {
         state = 0;
       }
-      
-
     }
   }
 }
